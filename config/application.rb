6d8f1cce997729b61fb8601770dbbcb0cbf6c6b2
slingod <slingo.drisca1@gmail.com>
2025-02-24 08:36:11 +0100

    bug fix and rework on my app.css

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module EventbriteApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0w
+    config.load_defaults 8.0
 
     # Configuration for the application, engines, and railties goes here.
     # These settings can be overridden in specific environments using the files
